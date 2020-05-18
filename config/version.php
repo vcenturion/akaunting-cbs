232d1facc02935ec72ed9a7ca2934dfc6fbad5d9
Victor Centurion <vcenturion@gmail.com>
2020-05-18 17:55:02 +0000

    Primer commit

---
+++ config/version.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'name'      =>  'Akaunting',
+
+    'code'      =>  'Argon',
+
+    'major'     =>  '2',
+
+    'minor'     =>  '0',
+
+    'patch'     =>  '11',
+
+    'build'     =>  '',
+
+    'status'    =>  'Stable',
+
+    'date'      =>  '2-May-2020',
+
+    'time'      =>  '21:00',
+
+    'zone'      =>  'GMT +3',
+
+];
